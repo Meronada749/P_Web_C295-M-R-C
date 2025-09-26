36e86d1ad96edf8ff888b730cea86d8745de8482
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-09-26 15:37:30 +0200

    feat(write): hasmany

---
+++ app/models/writer.ts
@@ -0,0 +1,18 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import Book from './book.js'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+
+export default class Writer extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @hasMany(() => Book)
+  declare book: HasMany<typeof Book>
+}
