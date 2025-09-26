dba7d3f3a56681c48a70ad6fb313040618e7cc9b
Meronada749 <meron.essayas@eduvaud.ch>
2025-09-26 19:48:48 +0200

    feat(validator): Add book validator

---
+++ app/validators/book.ts
@@ -0,0 +1,14 @@
+import vine from '@vinejs/vine'
+
+const booksValidator = vine.compile(
+  vine.object({
+    title: vine.string().minLength(2).maxLength(255),
+    numberOfPages: vine.number().withoutDecimals().positive().min(1),
+    pdfLink: vine.string().minLength(2).maxLength(255),
+    abstract: vine.string().minLength(2).maxLength(255),
+    editor: vine.string().minLength(2).maxLength(255),
+    editionYear: vine.number().withoutDecimals().positive().min(4),
+    imagePath: vine.string().minLength(2).maxLength(255),
+  })
+)
+export { booksValidator }
