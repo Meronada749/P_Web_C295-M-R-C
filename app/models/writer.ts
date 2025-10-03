23a387f4f7dfb226c8b7dcd49881eab0972ab2dc
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-10-03 11:32:30 +0200

    fix(writer model): remove date time

--- app/models/writer.ts
+++ app/models/writer.ts
@@ -1,4 +1,3 @@
-import { DateTime } from 'luxon'
 import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import Book from './book.js'
 import type { HasMany } from '@adonisjs/lucid/types/relations'
@@ -15,10 +14,4 @@ export default class Writer extends BaseModel {
 
   @hasMany(() => Book)
   declare book: HasMany<typeof Book>
-
-  @column.dateTime({ autoCreate: true })
-  declare createdAt: DateTime
-
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  declare updatedAt: DateTime
 }
