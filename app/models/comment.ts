df372a53f6eca79079024a99958e8c0fa21bdcad
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-10-03 12:07:06 +0200

    feat(comment model): add belongs

--- app/models/comment.ts
+++ app/models/comment.ts
@@ -1,13 +1,27 @@
-import { DateTime } from 'luxon'
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import User from './user.js'
+import Book from './book.js'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 
 export default class Comment extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
-  @column.dateTime({ autoCreate: true })
-  declare createdAt: DateTime
+  @column()
+  declare comment: string
 
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  declare updatedAt: DateTime
+  //fk
+  @column()
+  declare userId: number
+
+  @column()
+  declare bookId: number
+
+  //Relation : one book belongs to one category
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  //Relation : one book belongs to one writer
+  @belongsTo(() => Book)
+  declare book: BelongsTo<typeof Book>
 }
