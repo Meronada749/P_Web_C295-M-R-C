0e1b80a2504af92f67c3214fa46ddbc9a1acdff8
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-10-03 11:37:30 +0200

    refactor(category model): remove unused library

--- app/models/category.ts
+++ app/models/category.ts
@@ -1,4 +1,3 @@
-import { DateTime } from 'luxon'
 import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
 import type { HasMany } from '@adonisjs/lucid/types/relations'
 import Book from './book.js'
