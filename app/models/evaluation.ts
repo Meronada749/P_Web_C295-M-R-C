9e22b032aa4d5a34b0bf4d675133bb0c12b7ac78
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-10-03 11:32:08 +0200

    fix(evaluation model): remove date time

--- app/models/evaluation.ts
+++ app/models/evaluation.ts
@@ -1,14 +1,9 @@
-import { DateTime } from 'luxon'
 import { BaseModel, column } from '@adonisjs/lucid/orm'
 
-export default class Evalutation extends BaseModel {
+export default class Evaluation extends BaseModel {
   @column({ isPrimary: true })
   declare id: number
 
-
-  @column.dateTime({ autoCreate: true })
-  declare createdAt: DateTime
-
-  @column.dateTime({ autoCreate: true, autoUpdate: true })
-  declare updatedAt: DateTime
+  @column()
+  declare note: number
 }
