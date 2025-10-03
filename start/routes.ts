31ef37ca03f11c3786f39fb470e89096cb80ad9b
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-10-03 12:08:37 +0200

    feat(routes): route for comments

--- start/routes.ts
+++ start/routes.ts
@@ -9,6 +9,7 @@
 
 import BooksController from '#controllers/books_controller'
 import CategoriesController from '#controllers/categories_controller'
+import CommentsController from '#controllers/comments_controller'
 import EvaluationsController from '#controllers/evaluations_controller'
 import WritersController from '#controllers/writers_controller'
 import router from '@adonisjs/core/services/router'
@@ -23,3 +24,4 @@ router.resource('categories', CategoriesController).apiOnly()
 router.resource('books', BooksController).apiOnly()
 router.resource('writers', WritersController).apiOnly()
 router.resource('evaluations', EvaluationsController).apiOnly()
+router.resource('comments', CommentsController).apiOnly()
