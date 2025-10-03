37eada492a09d52011aac7c9d20d4663c03fbfb0
Chris0xdeadbeef <christopher.ristic@yahoo.fr>
2025-10-03 11:50:55 +0200

    feat(evaluation model): add model evaluations

--- app/models/evaluation.ts
+++ app/models/evaluation.ts
@@ -1,4 +1,7 @@
-import { BaseModel, column } from '@adonisjs/lucid/orm'
+import { BaseModel, column, belongsTo } from '@adonisjs/lucid/orm'
+import User from './user.js'
+import Book from './book.js'
+import type { BelongsTo } from '@adonisjs/lucid/types/relations'
 
 export default class Evaluation extends BaseModel {
   @column({ isPrimary: true })
@@ -6,4 +9,19 @@ export default class Evaluation extends BaseModel {
 
   @column()
   declare note: number
+
+  //fk
+  @column()
+  declare userId: number
+
+  @column()
+  declare bookId: number
+
+  //Relation : one book belongs to one category
+  @belongsTo(() => User)
+  declare user: BelongsTo<typeof User>
+
+  //Relation : one book belongs to one writer
+  @belongsTo(() => Book)
+  declare book: BelongsTo<typeof Book>
 }
